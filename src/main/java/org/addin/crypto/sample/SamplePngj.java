5555f3bc9a31dd1e13826024811995c222d13dd2
Addins <Addins@bitbucket.org>
2015-05-25 00:04:22 +0700

    just playing around.

--- src/main/java/org/addin/crypto/sample/SamplePngj.java
+++ src/main/java/org/addin/crypto/sample/SamplePngj.java
@@ -8,6 +8,7 @@ import ar.com.hjg.pngj.PngWriter;
 import ar.com.hjg.pngj.chunks.ChunkCopyBehaviour;
 import ar.com.hjg.pngj.chunks.PngChunkTextVar;
 import java.io.File;
+import java.security.SecureRandom;
 import org.addin.crypto.classic.core.SimpleKey;
 import org.addin.crypto.classic.image.ImageLineIntSuperEncryption;
 
@@ -21,8 +22,9 @@ public class SamplePngj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        testEncryptImage();
-        testDecryptImage();
+//        testEncryptImage();
+//        testDecryptImage();
+        testGenerateMatrix();
     }
 
     public static void sample() {
@@ -107,6 +109,14 @@ public class SamplePngj {
         pngr.end();
         pngw.end();
     }
+    
+    public static void testGenerateMatrix(){
+        int[][] mtx = generateSquareMatrix(16);
+        printMatrix(mtx);
+        System.out.println("---------------------------");
+        mtx = generateSquareMatrix(16);
+        printMatrix(mtx);
+    }
 
     public static int[][] getDiagonalMatrix(int size) {
         int count = 0;
@@ -126,6 +136,43 @@ public class SamplePngj {
             }
             System.out.println();
         }
+    }
+    
+    public static int[][] generateSquareMatrix(int size){
+        int[] domain = getIntFromZeroTo(size*size);
+        shufflingElements(domain);
+        
+        int[][] mtx = new int[size][size];
+        for (int i = 0, idx = 0; i < mtx.length; i++) {
+            for (int j = 0; j < mtx[i].length; j++, idx++) {
+                mtx[i][j] = domain[idx];
+            }            
+        }
+        return mtx;
+    }
+
+    public static int[] getIntFromZeroTo(int to) {
+        int[] output = new int[to];
+        for (int i = 0; i < to; i++) {
+            output[i] = i;
+        }
+        return output;
+    }
+
+    /**
+     * implementation of Fisher-Yates Shuffle
+     * @param input
+     * @return 
+     */
+    public static int[] shufflingElements(int[] input) {
+        SecureRandom sr = new SecureRandom();
+        for (int i = input.length - 1; i > 0; i--) {
+            int idx = sr.nextInt(input.length);
+            int a = input[idx];
+            input[idx] = input[i];
+            input[i] = a;
+        }
+        return input;
     }
 
 }
