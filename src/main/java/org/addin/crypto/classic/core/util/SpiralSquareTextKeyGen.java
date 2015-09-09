c50b26fa369c14bfcb3e88b3435b5ffe255b1dc1
Addinul Kintangi <addins3009@gmail.com>
2015-09-09 20:54:25 +0700

    add another key generation method based on string (text).

---
+++ src/main/java/org/addin/crypto/classic/core/util/SpiralSquareTextKeyGen.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.addin.crypto.classic.core.util;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ *
+ * @author addin <addins3009@gmail.com>
+ */
+public class SpiralSquareTextKeyGen extends TextKeyGen{
+
+    public SpiralSquareTextKeyGen(int size, String text) {
+        super(size, text);
+    }
+
+    @Override
+    public int[] shuffling() {
+        int[] initial = super.shuffling();
+        int[] reversed = new int[initial.length];
+        
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = initial[initial.length-1-i];            
+        }
+        domain = reversed;
+        return reversed;
+    }
+
+    @Override
+    public int[][] generateMatrix() {
+        int[][] initialMtx = super.generateMatrix();
+        return makeSpiralSquareArray(initialMtx, size);
+    }
+    
+    private int[][] makeSpiralSquareArray(int[][] input, int mXn){
+        int top = 0, bottom = mXn-1, left = 0, right = mXn-1;
+        int direction = 0; // 0 = to the right, 1 = to the bottom, 2 = to the left, 3 = upward.
+        int[][] res = new int[mXn][mXn];
+        int x = 0,y =0;
+        
+        while (top <= bottom && left <= right) {            
+            if(direction == 0){
+                for (int i = left; i <= right; i++) {
+//                    System.out.println(input[top][i]);
+                    res[x][y] = input[top][i];
+                    y++;
+                    if(y==mXn){
+                        y=0;
+                        x++;
+                    }
+                    if(x==mXn)
+                        break;
+                }
+                top++;
+            }else if(direction == 1){
+                for (int i = top; i <= bottom; i++) {
+//                    System.out.println(input[i][right]);
+                    res[x][y] = input[i][right];
+                    y++;
+                    if(y==mXn){
+                        y=0;
+                        x++;
+                    }
+                    if(x==mXn)
+                        break;
+                }
+                right--;
+            }else if(direction == 2){
+                for (int i = right; i >= left; i--) {
+//                    System.out.println(input[bottom][i]);
+                    res[x][y] = input[bottom][i];
+                    y++;
+                    if(y==mXn){
+                        y=0;
+                        x++;
+                    }
+                    if(x==mXn)
+                        break;
+                }
+                bottom--;
+            }else if(direction == 3){
+                for (int i = bottom; i >= top; i--) {
+//                    System.out.println(input[i][left]);
+                    res[x][y] = input[i][left];
+                    y++;
+                    if(y==mXn){
+                        y=0;
+                        x++;
+                    }
+                    if(x==mXn)
+                        break;
+                }
+                left++;
+            }
+            direction = (direction+1)%4;
+        }
+        return res;
+    }
+}
---
+++ src/test/java/org/addin/crypto/classic/core/util/SpiralSquareTextKeyGenTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.addin.crypto.classic.core.util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author addin <addins3009@gmail.com>
+ */
+public class SpiralSquareTextKeyGenTest {
+    
+    public SpiralSquareTextKeyGenTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of generateMatrix method, of class SpiralSquareTextKeyGen.
+     */
+    @Test
+    public void testGenerateMatrix() {
+        System.out.println("generateMatrix");
+        SpiralSquareTextKeyGen instance = new SpiralSquareTextKeyGen(16,"a");
+        int[][] expResult = new int[][]{{8,7,6},{3,0,1},{2,5,4}};
+        int[][] result = instance.generateMatrix();
+        print2dMatrix(result);
+        assertArrayEquals(expResult, result);
+    }
+    
+    public static void print2dMatrix(int[][] input){
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                System.out.print(input[i][j]+", ");
+            }
+            System.out.println("");
+        }
+    }
+}
